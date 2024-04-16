50b995caa4f6a435653c53c1d743e4c9efea4c94
Antonio Díaz <20223TN047@utez.edu.mx>
2024-04-15 20:52:58 -0600

    Refactor Usuarios component: - Update import statements to use double quotes instead of single quotes - Add useEffect hook to fetch users data on component mount - Update filteredUsuarios logic to filter users based on name instead of correo and nombreUsuario - Update table rendering logic to handle empty users array - Update table row rendering logic to display user name and username - Update link in table row to use user id for dynamic routing

--- src/components/layout/screen/Usuarios.jsx
+++ src/components/layout/screen/Usuarios.jsx
@@ -1,31 +1,46 @@
-import React, { useState } from 'react';
-import { Button } from 'flowbite-react';
-import Imagen from '../../../assets/logo.png';
-import './estilos/style.css';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Button } from "flowbite-react";
+import Imagen from "../../../assets/logo.png";
+import "./estilos/style.css";
+import { Link } from "react-router-dom";
 
 const Usuarios = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [usuarios, setUsuarios] = useState([
-    { correo: 'asasa@sas', nombreUsuario: 'hjhjjhj', area: 'ddsdsdsd' },
-    { correo: 'dsdss@ass', nombreUsuario: 'sasassa', area: 'asawqssa'}
-  ]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [usuarios, setUsuarios] = useState([]);
+
+  const getToken = () => {
+    console.log(localStorage.getItem("token"));
+    return localStorage.getItem("token");
+  };
+
+  //Obtenemos usuarios:
+  const getUsuarios = async () => {
+    const token = getToken();
+    const response = await fetch("http://localhost:8080/api/person/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data.data);
+    setUsuarios(data.data);
+  };
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
   };
 
-  const filteredUsuarios = usuarios.filter(usuario =>
-    usuario.correo.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    usuario.nombreUsuario.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  useEffect(() => {
+    getUsuarios();
+  }, []);
 
   return (
     <div>
-      {/* Navbar SIMNA cpr*/}
       <nav className="bg-white w-full p-4 flex justify-between items-center">
-        <div className="flex justify-center flex-1"> 
-          <div className="flex items-center"> 
+        <div className="flex justify-center flex-1">
+          <div className="flex items-center">
             <input
               type="text"
               placeholder="Buscar Usuario"
@@ -36,38 +51,62 @@ const Usuarios = () => {
           </div>
         </div>
       </nav>
-      {/* Tabla de Usuarios */}
       <div className="p-4 mx-auto mt-8 max-w-4xl">
         <table className="w-full border-collapse border border-gray-300 bg-white">
           <thead>
             <tr>
-              <th className="correo">Correo</th>
               <th className="nombreUsuario">Nombre de Usuario</th>
-              <th className="area">Área</th>
+              <th className="area">Rol:</th>
               <th className="acciones">Acciones</th>
             </tr>
           </thead>
           <tbody>
-            {filteredUsuarios.map((usuario, index) => (
-              <tr key={index} className={index % 2 === 0 ? 'bg-gray-100' : ''}>
-                <td className="border border-gray-300 px-4 py-2">{usuario.correo}</td>
-                <td className="border border-gray-300 px-4 py-2">{usuario.nombreUsuario}</td>
-                <td className="border border-gray-300 px-4 py-2">{usuario.area}</td>
-                <td className="border border-gray-300 px-4 py-1 flex justify-between items-center">
-                  <Button style={{ background: "#5790AB" }} className='shadow btn-sm' type="submit">
-                    <Link to="/modificacionusuario">Modificar</Link>
-                  </Button>
-                  <Button style={{ background: "#5790AB" }} className='shadow btn-sm' type="submit">
-                    <Link to="/eliminausuario">Eliminar</Link>
-                  </Button>
+            {Array.isArray(usuarios) && usuarios.length > 0 ? (
+              usuarios
+                .filter((usuario) => {
+                  if (searchTerm === "") {
+                    return usuario;
+                  } else if (
+                    usuario.name
+                      .toLowerCase()
+                      .includes(searchTerm.toLowerCase())
+                  ) {
+                    return usuario;
+                  }
+                })
+                .map((usuario, index) => (
+                  <tr
+                    key={index}
+                    className={index % 2 === 0 ? "bg-gray-100" : ""}
+                  >
+                    <td className="border border-gray-300 px-4 py-2">
+                      {usuario.name} {usuario.surname}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2">
+                      {usuario.user.roles[0].name}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2">
+                      <Link
+                        to={`/modificacion-usuario/${usuario.id}`}
+                        className="text-blue-500"
+                      >
+                        Editar
+                      </Link>
+                    </td>
+                  </tr>
+                ))
+            ) : (
+              <tr>
+                <td colSpan="3" className="border border-gray-300 px-4 py-2">
+                  No hay usuarios
                 </td>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
       </div>
     </div>
   );
-}
+};
 
 export default Usuarios;
