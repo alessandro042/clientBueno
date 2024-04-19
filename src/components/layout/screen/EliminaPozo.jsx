51517f919e54cca7edccb52800c80e96c4dfe0ac
Irving Uriel Espinosa Hernandez <20223tn050@utez.edu.mx>
2024-04-19 10:38:51 -0600

    pUSH

--- src/components/layout/screen/EliminaPozo.jsx
+++ src/components/layout/screen/EliminaPozo.jsx
@@ -1,66 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'flowbite-react';
 import Imagen from '../../../assets/logo.png';
 import './estilos/style.css';
 import { Link } from 'react-router-dom';
 
 const EliminaPozo = () => {
-    const usuarios = [
-        { correo: 'Capacidad', dato: '1100 L', nombre: 'Ubicación', dato2: 'Calle Gardenias Col. Azteca Temixco Mor', rol: 'Nivel', dato3: '70%' }
-    ];
+    const [pozo, setPozo] = useState(null);
+
+    // Obtenemos token y ID de pozo
+    const getToken = () => localStorage.getItem("token");
+    const getIdPozo = () => localStorage.getItem("id");
+    console.log("id pozo", getIdPozo());
+    console.log("token", getToken());
+
+    // Obtener datos del pozo
+    const getPozo = async () => {
+        const token = getToken();
+        const id = getIdPozo();
+        const response = await fetch(`http://localhost:8080/api/pozos/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+        setPozo(data);
+    };
+
+    // Funcion para eliminar pozo
+    const deletePozo = async () => {
+        const token = getToken();
+        const id = getIdPozo();
+        const response = await fetch(`http://localhost:8080/api/pozos/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+    };
+
+    useEffect(() => {
+        getPozo();
+    }, []);
 
     return (
         <div>
-            {/* Navbar SIMNA cpr*/}
-            <nav className="bg-blue-900 w-full p-4 flex justify-between items-center" style={{ background: "#072D44" }}>
-                <div className="flex items-center">
-                    <img src={Imagen} alt="Logo" className="h-12 w-auto mr-4" />
-                    <h1 className="text-white font-bold text-lg">SIMNA</h1>
-                </div>
-
-                <div className="flex items-center">
-                    <button className="text-white focus:outline-none">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M4 6h16M4 12h16m-7 6h7"
-                            />
-                        </svg>
-                    </button>
-                </div>
-            </nav>
-
-
             <div className="p-4 mx-auto mt-8 max-w-4xl">
                 <table className="w-full border-collapse border border-gray-300 bg-white">
                     <thead>
                         <tr>
-                            <th className="correo">Capacidad</th>
-                            <th className="nombre">Ubicación</th>
-                            <th className="rol">Nivel</th>
+                            <tr>
+                                <th className="nombre">Nombre Pozo</th>
+                                <th className="capacidadLitros">Capacidad</th>
+                                <th className="profundidad">Profundidad</th>
+                                <th className="porcentajeAgua">Porcentaje Agua</th>
+                                <th className="ubicacionPozo">Ubicacion</th>
+                                <th className="comunidades">Comunidades</th>
+                                <th className="estatus">Estatus</th>
+                            </tr>
                         </tr>
                     </thead>
                     <tbody>
-                        {usuarios.map((usuario, index) => (
-                            <tr key={index} className={index % 2 === 0 ? 'bg-gray-100' : ''}>
-                                <td className="border border-gray-300 px-4 py-2">{usuario.correo}: {usuario.dato}</td>
-                                <td className="border border-gray-300 px-4 py-2">{usuario.nombre}: {usuario.dato2}</td>
-                                <td className="border border-gray-300 px-4 py-2">{usuario.rol}: {usuario.dato3}</td>
+                    {pozo && (
+                            <tr key={pozo.id}>
+                                <td>{pozo && pozo.nombre}</td>
+                                <td>{pozo && pozo.capacidadLitros}</td>
+                                <td>{pozo && pozo.profundidad}</td>
+                                <td>{pozo && pozo.porcentajeAgua}</td>
+                                <td>{pozo && pozo.ubicacionPozo}</td>
+                                <td>{pozo && pozo.comunidades}</td>
+                                <td>{pozo && pozo.estatus}</td>
                             </tr>
-                        ))}
+                        )}
                     </tbody>
                 </table>
                 <div className="flex justify-center mt-4">
-                    <Button className="bg-blue-600 text-white px-4 py-2" style={{ backgroundColor: '#5790AB' }}>
-                        <Link to="/pozos">Eliminar</Link>
+                    <Button className="bg-blue-600 text-white px-4 py-2" style={{ backgroundColor: '#5790AB' }} onClick={deletePozo}>
+                        <Link to="/pozos">Eliminar pozo</Link>
                     </Button>
                 </div>
             </div>
