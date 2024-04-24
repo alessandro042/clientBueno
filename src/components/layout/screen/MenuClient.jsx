1b81461e97329881b50880d9ab068265262cc8ff
Irving Uriel Espinosa Hernandez <20223tn050@utez.edu.mx>
2024-04-24 01:04:03 -0600

    Se hizo el filtrado de usuarios

---
+++ src/components/layout/screen/MenuClient.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Button, Card } from "flowbite-react";
+import { Link, Outlet } from "react-router-dom";
+
+const MenuClient = () => {
+    const cerrarSesion = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        window.location.href = "/";
+    };
+
+    return (
+        <div className="flex flex-col lg:flex-row h-screen">
+            <div
+                style={{ background: "#072D44" }}
+                className="w-full flex justify-center items-center bg-blue-900"
+            >
+                <Card
+                    className="max-w-md border-none shadow-lg rounded-lg text-center transition-transform duration-300 transform hover:scale-105"
+                    style={{ background: "#064469", padding: "40px", boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)" }}
+                >
+                    <div className="font text-3xl font-bold mb-6 text-white">
+                        Bienvenido!
+                    </div>
+                    <div className="flex flex-col gap-7 items-center">
+                        <Link to="/pozos">
+                            <Button
+                                style={{ background: "#072D44", width: "100%", minWidth: "250px" }}
+                                className="shadow-lg text-xl transition-transform duration-300 transform hover:scale-105"
+                            >
+                                Gestionar Pozos
+                            </Button>
+                        </Link>
+                        <Button
+                            style={{ background: "#072D44", width: "100%", minWidth: "250px" }}
+                            className="shadow-lg text-xl transition-transform duration-300 transform hover:scale-105"
+                            onClick={cerrarSesion}
+                        >
+                            Salir
+                        </Button>
+                    </div>
+                </Card>
+            </div>
+        </div>
+    );
+};
+
+export default MenuClient;
---
+++ src/components/layout/screen/PozosClient.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+const PozosClient = () => {
+    const [pozo, setPozo] = useState(null);
+
+    const getToken = () => {
+        return localStorage.getItem("token");
+    };
+
+    // Get de pozos
+    const getPozos = async () => {
+        try {
+            const response = await axios.get("http://localhost:8080/api/pozos", {
+                headers: {
+                    Authorization: `Bearer ${getToken()}`,
+                },
+            });
+            console.log(response.data);
+            setPozo(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    // Actualizacion de pozo con put
+    const putPozo = async (id) => {
+        const token = getToken();
+        const array = id - 1;
+        console.log(
+            "Actualizando pozo con capacidadlitros: ",
+            pozo[array].capacidadLitros
+        );
+        try {
+            console.log("Actualizando pozo con id: ", array);
+            console.log("Token: ", token);
+            const response = await fetch(`http://localhost:8080/api/pozos/update/${id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    capacidadLitros: pozo[array].capacidadLitros,
+                    comunidades: pozo[array].comunidades,
+                    nombre: pozo[array].nombre,
+                    ubicacionPozo: pozo[array].ubicacionPozo,
+                    porcentajeAgua: pozo.porcentajeAgua,
+                    estatus: pozo.estatus,
+                }),
+            });
+            if (response.status === 200) {
+                window.location.href = "/pozos";
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        getPozos();
+    }, []);
+
+    return (
+        <div className="p-4 mx-auto mt-8 max-w-4xl">
+            <table className="w-full border-collapse border border-gray-300 bg-white">
+                <thead>
+                    <tr>
+                        <th className="nombre">Nombre</th>
+                        <th className="capacidad">Capacidad Litros</th>
+                        <th className="porcentaje">Porcentaje Agua</th>
+                        <th className="ubicacion">Ubicación</th>
+                        <th className="comunidades">Comunidades</th>
+                        <th className="acciones">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Array.isArray(pozo) &&
+                        pozo.map((pozo) => (
+                            <tr key={pozo.id}>
+                                <td>{pozo.nombre}</td>
+                                <td>{pozo.capacidadLitros} L</td>
+                                <td>{pozo.porcentajeAgua} %</td>
+                                <td>{pozo.ubicacionPozo}</td>
+                                <td>{pozo.comunidades}</td>
+                                <td>
+                                    <Button
+                                        style={{ background: "#5790AB" }}
+                                        className="shadow btn-sm w-auto h-auto px-1 py-0.5 text-sm"
+                                        onClick={() => putPozo(pozo.id)}
+                                    >
+                                        Actualizar
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default PozosClient;
